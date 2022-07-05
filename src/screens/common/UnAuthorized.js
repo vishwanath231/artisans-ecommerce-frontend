74b8d7d95a066db5c2a7568584f23708a23ec163
Vishwanath <vishwanathvishwabai@gmail.com>
2022-07-06 00:32:38 +0530

    Update UnAuthorized.js

--- src/screens/common/UnAuthorized.js
+++ src/screens/common/UnAuthorized.js
@@ -12,8 +12,10 @@ const UnAuthorized = () => {
                 <div className='text-7xl font-black text-[#c91237] mb-4'>401</div>
                 <div className='text-3xl font-bold'>UnAuthorized</div>
             </div>
-            <div className='text-center>
-               <Link to='/' className='text-base font-medium py-2 px-4 mont-font rounded shadow'>Go Back</Link>
+            <div className='text-center'>
+               <Link to='/' className='text-base font-medium py-2 px-4 mont-font rounded shadow'>
+                   Go Back
+               </Link>
             </div>
         </div>
     )
